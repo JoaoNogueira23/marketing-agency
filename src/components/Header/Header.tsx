ceecd961bd2397b94b6034b6e8840a2113bdd2e2
JoaoNogueira23 <joao.n.m23@gmail.com>
2024-09-02 20:48:00 -0300

    feat: feature header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,17 +4,15 @@ export default function Header() {
     return(
         <header className="header">
             <h2 className='logo'>
-                Alchemy
+                Agency
             </h2>
 
             <nav className='nav'>
                 <ul>
-                    <li>About Us</li>
-                    <li>Services & Pricing</li>
-                    <li>
-                        Blog
-                    </li>
-                    <li>Contact Us</li>
+                    <li><a href='#'>About Us</a></li>
+                    <li><a href='#'>Services & Pricing</a></li>
+                    <li><a href='#'>Blog</a></li>
+                    <li><a href='#'>Contact Us</a></li>
                 </ul>
             </nav>
 
