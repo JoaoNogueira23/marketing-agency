e4dc3a49f3d0fd65849ef5c5174759d7bf6ba201
JoaoNogueira23 <jvictor.n.m23@gmail.com>
2024-09-24 11:37:00 -0300

    feat: integration request user login

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,9 @@
 "use client"
-import { useState } from 'react'
 
 import '../../styles/layouts/stylesForms.sass'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import axios from 'axios'
+import { useState } from 'react'
 
 interface AdminPageProps {
 
@@ -19,21 +19,38 @@ const defaultValue: IFormInput = {
     password: ''
 }
 
-export default async function AdminPage(props : AdminPageProps){
+export default function AdminPage(props : AdminPageProps){
+    const [loginError, setLoginError] = useState<string>('')
     const {register, handleSubmit, formState: {errors}} = useForm<IFormInput>();
 
    
     const handlerRequestLogin = (data: IFormInput) => {
-        const url = 'localhost:8080/api'
+        const url = 'http://localhost:8080/api'
 
-        const payload = {
+        const payloadJSON ={
             username: data.usermail,
             password: data.password
         }
     
-        axios.post(url + '/user/login', payload)
+        fetch(
+            url + '/user/login',{
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/x-www-form-urlencoded'
+              },    
+              body: new URLSearchParams({
+                  'username': data.usermail,
+                  'password': data.password,
+                  'grant_type': 'password'
+              })
+            }
+        )
             .then((response) => {
-                console.log(response)
+                if(response.status == 200){
+                    alert("Login realizado com sucesso")
+                }else if(response.status == 401){
+                    alert("User not registered!")
+                }
             })
             .catch((err) => {
                 console.log(err)
