62a8f7dcfb8953f4a9dd01533a55a52cd20a3350
JoaoNogueira23 <jvictor.n.m23@gmail.com>
2024-10-02 22:16:13 -0300

    feat: link home page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,29 @@
+import Link from 'next/link'
 import './header.sass'
 
 export default function Header() {
     return(
         <header className="header">
-            <h2 className='logo'>
-                Agency
+            <h2>
+                <Link href={'/'} className='logo'>
+                    Agency
+                </Link>
             </h2>
 
             <nav className='nav'>
                 <ul>
-                    <li><a href='#'>About Us</a></li>
-                    <li><a href='#'>Services & Pricing</a></li>
-                    <li><a href='#'>Blog</a></li>
-                    <li><a href='#'>Contact Us</a></li>
+                    <li>
+                        <a href='#'>About Us</a>
+                    </li>
+                    <li>
+                        <a href='#'>Services & Pricing</a>
+                    </li>
+                    <li>
+                        <a href='#'>Blog</a>
+                    </li>
+                    <li>
+                        <a href='#'>Contact Us</a>
+                    </li>
                 </ul>
             </nav>
 
