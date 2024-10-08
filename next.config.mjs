93b7470d2a75aaa6a2706e6d98433ca43c7c29e1
JoaoNogueira23 <jvictor.n.m23@gmail.com>
2024-10-08 20:29:22 -0300

    build: config nextjs for requests external images

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  trailingSlash: true,
+  images: {
+      formats: ['image/webp'],
+      remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'storage.cloud.google.com',
+            port: '',
+            pathname: '/blog-content-s3/**',
+          },
+        ],
+    },
 };
 
 export default nextConfig;
