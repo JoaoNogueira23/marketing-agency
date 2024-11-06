316c8249792cc35b90a58b8983e971dd7ae6f6da
JoaoNogueira23 <jvictor.n.m23@gmail.com>
2024-11-06 14:20:40 -0300

    refactor: styles manager

--- src/pages/Home/Sections/Testimonials/TestimonialsSection.tsx
+++ src/pages/Home/Sections/Testimonials/TestimonialsSection.tsx
@@ -1,21 +1,20 @@
 import Image from 'next/image'
-import styles from './styles.module.sass'
 
 export default function TestimonialsSection() {
     return(
-        <div className={styles.containerTestimonialsSection}>
-            <h5 className={styles.titlePrimary}>
+        <div className={"containerTestimonialsSection"}>
+            <h5 className={"titlePrimary"}>
                 Testimonials
             </h5>
 
-            <h3 className={styles.titleSecondary}>
+            <h3 className={"titleSecondary"}>
                 What clients are say
             </h3>
 
-            <div className={styles.cardsTestimonials}>
+            <div className={"cardsTestimonials"}>
 
-                <div className={styles.cardTestimonial}>
-                    <h4 className={styles.titleCard}>
+                <div className={"cardTestimonial"}>
+                    <h4 className={"titleCard"}>
                         It has been a great help by Agency
                     </h4>
 
@@ -25,21 +24,21 @@ export default function TestimonialsSection() {
                         traffic and leads
                     </p>
 
-                    <div className={styles.profileUser}>
+                    <div className={"profileUser"}>
                         <Image 
                         src={'/profile1.webp'}
                         width={50}
                         height={50}
                         alt='Profile Image'
-                        className={styles.imgUser}
+                        className={"imgUser"}
                         />
 
-                        <div className={styles.contentInfoUser}>
-                            <h4 className={styles.useName}>
+                        <div className={"contentInfoUser"}>
+                            <h4 className={"useName"}>
                                 John Smith
                             </h4>
 
-                            <p className={styles.userPosition} >
+                            <p className={"userPosition"} >
                                 CEO of Company X
                             </p>
                         </div>
@@ -47,7 +46,7 @@ export default function TestimonialsSection() {
                     </div>
                 </div>
 
-                <div className={styles.cardTestimonial}>
+                <div className={"cardTestimonial"}>
                     <h4>
                         It has been a great help by Agency
                     </h4>
@@ -57,21 +56,21 @@ export default function TestimonialsSection() {
                         marketing compaign. We saw a significant incrase in website
                         traffic and leads
                     </p>
-                    <div className={styles.profileUser}>
+                    <div className={"profileUser"}>
                         <Image 
                         src={'/profile2.webp'}
                         width={50}
                         height={50}
                         alt='Profile Image'
-                        className={styles.imgUser}
+                        className={"imgUser"}
                         />
 
-                        <div className={styles.contentInfoUser}>
-                            <h4 className={styles.useName}>
+                        <div className={"contentInfoUser"}>
+                            <h4 className={"useName"}>
                                 Jane Doe
                             </h4>
 
-                            <p className={styles.userPosition} >
+                            <p className={"userPosition"} >
                                 CEO of Company Y
                             </p>
                         </div>
